09cdad2723405e835185c3b8d74556ca7dc767cb
Will Coulter <will.coulter01@stclairconnect.ca>
2018-04-06 01:54:36 -0400

    #21 Made updates to the spinners

--- app/src/main/java/wc/productions/gamekeeper/CreateGameFragment.java
+++ app/src/main/java/wc/productions/gamekeeper/CreateGameFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 
 /**
@@ -26,6 +29,9 @@ public class CreateGameFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    Spinner spinTeamOne;
+    Spinner spinTeamTwo;
 
     private OnFragmentInteractionListener mListener;
 
@@ -63,8 +69,15 @@ public class CreateGameFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_create_game, container, false);
+        MainActivity.fab.hide();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_game, container, false);
+        spinTeamOne = view.findViewById(R.id.firstTeamSpinner);
+        spinTeamTwo = view.findViewById(R.id.secondTeamSpinner);
+        DatabaseHandler db = new DatabaseHandler(getContext());
+        ArrayList<Team> list = db.getAllTeams();
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
